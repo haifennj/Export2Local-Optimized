3a6136dc9faa4285e15739e793f0c6890fa51d30
GitHub Action <action@github.com>
2023-05-14 13:38:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/haifennj/export2localoptimized/MyBundle.kt
+++ src/main/kotlin/com/github/haifennj/export2localoptimized/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.haifennj.export2localoptimized
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/haifennj/export2localoptimized/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/haifennj/export2localoptimized/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.haifennj.export2localoptimized.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.haifennj.export2localoptimized.MyBundle
+import com.github.haifennj.export2localoptimized.services.MyProjectService
 import javax.swing.JButton
 
 
